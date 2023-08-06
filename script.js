3a5b27c90437b2ca77933bb8fee1873a528e028b
Sarah Goodell <sarahlgoodell1@gmail.com>
2023-08-06 16:34:05 -0400

    started to create function for city search button

--- script.js
+++ script.js
@@ -4,14 +4,13 @@ var searchTextEl = document.getElementById("cityName");
 console.log(searchTextEl)
 // Selects Search Button
 var searchButton = document.getElementsByClassName("btn-primary");
-// Selects area to append gray city buttons to append to
-var emptyDiv = document.getElementById("emptyDiv");
 // Selects City Name Box Header
 var cityNameInfo = document.getElementById("cityNameInfo").firstChild;
 // Selects list of city name elements
 var cityList = document.getElementById("cityList");
 // Selects 5 Day Forecast Boxes
 var fiveDayBoxes = document.getElementsByClassName("rounded");
+
 
 // Toggles all buttons using Bootstrap
 var buttons = document.querySelectorAll('.btn')
@@ -26,13 +25,27 @@ buttons.forEach(function (button) {
 
 var getWeather = () => {
   // Hard coded in Chicago to test it
-  fetch(api.openweathermap.org/data/2.5/forecast?q=Chicago&appid:b98ec477e026dbcba46222f669c18788)
+  fetch(api.openweathermap.org/data/2.5/forecast?q=Chicago&appid=b98ec477e026dbcba46222f669c18788)
   .then(response => response.json())
   // Don't know what to put here???
   .then()
 }
 
 // Get Weather By City Search Bar
+searchButton.addEventListener(`search`, (event) => {
+  event.preventdefault()
+  var cityNameValue = searchTextEl.value;
 
+  console.log(`User requested weather for ${cityNameValue}`)
+
+  // Clears everything on the right side
+  searchTextEl.innerHTML = ``;
+  cityNameInfo.innerHTML = ``;
+  cityList.innerHTML = ``;
+  fiveDayBoxes.innerHTML = ``;
+
+  // getWeather(`url to get weather`)
+
+})
 // Get Weather by Clicking a Gray Button
 
