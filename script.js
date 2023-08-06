bf30a4b504e61f8168e47f9e3b73ced63f7484b6
Sarah Goodell <sarahlgoodell1@gmail.com>
2023-08-06 16:16:18 -0400

    Added styling to left column of website

--- script.js
+++ script.js
@@ -13,12 +13,26 @@ var cityList = document.getElementById("cityList");
 // Selects 5 Day Forecast Boxes
 var fiveDayBoxes = document.getElementsByClassName("rounded");
 
-
-
-// Toggles all buttons
+// Toggles all buttons using Bootstrap
 var buttons = document.querySelectorAll('.btn')
 buttons.forEach(function (button) {
   var button = new bootstrap.Button(button)
   button.toggle()
 });
+
+// Geocoding API
+// Coverts city names to coordinates
+// api.openweathermap.org/data/2.5/forecast?q={city name}&appid=b98ec477e026dbcba46222f669c18788
+
+var getWeather = () => {
+  // Hard coded in Chicago to test it
+  fetch(api.openweathermap.org/data/2.5/forecast?q=Chicago&appid:b98ec477e026dbcba46222f669c18788)
+  .then(response => response.json())
+  // Don't know what to put here???
+  .then()
+}
+
+// Get Weather By City Search Bar
+
+// Get Weather by Clicking a Gray Button
 
