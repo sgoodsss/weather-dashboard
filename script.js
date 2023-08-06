a8662e001ab879029628fd7e074d8799e2f1d04e
Sarah Goodell <sarahlgoodell1@gmail.com>
2023-08-06 19:31:20 -0400

    Created pseudocode for JS page

--- script.js
+++ script.js
@@ -1,3 +1,24 @@
+// TODO:
+// when user searches for a city (clicks search button):
+//  - store the user input in a variable
+//  - use a fetch api to get the current & future conditions for that city
+//  - store that city into local storage
+
+// use the data from fetch to populate in the current-weather container:
+//  - name and today's date as M/DD/YYY
+//  - an icon reprsentation of weather conditions
+//  - temp
+//  - wind
+//  - humidity
+
+// use the data from fetch to populate in the five-day container:
+//  - date
+//  - an icon reprsentation of weather conditions
+//  - the temp
+//  - wind speed
+
+//  - when you click the button it displays the current and future conditions for that city
+
 // GLOBAL VARIABLES 
 // Selects City Name Input
 var searchTextEl = document.getElementById("cityName");
