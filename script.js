55d90a200530e513d503b5aed929934d74d3be50
Sarah Goodell <sarahlgoodell1@gmail.com>
2023-08-06 20:35:31 -0400

    Put api promise in my code

--- script.js
+++ script.js
@@ -1,8 +1,53 @@
+// GLOBAL VARIABLES 
+// Selects City Name Input
+var searchTextEl = document.getElementById("cityName");
+console.log(searchTextEl)
+// Selects Search Button
+var searchButton = document.getElementsByClassName("btn-primary");
+// Selects City Name Box Header
+var cityNameInfo = document.getElementById("cityNameInfo").firstChild;
+// Selects list of city name elements
+var cityList = document.getElementById("cityList");
+// Selects 5 Day Forecast Boxes
+var fiveDayBoxes = document.getElementsByClassName("rounded");
+// My Personal API Key
+var openWeatherApiKey = 'b98ec477e026dbcba46222f669c18788';
+// URL to fetch data by city name
+var openWeatherCityUrl = 'https://api.openweathermap.org/data/2.5/forecast?q=';
+
+// Geocoding API
+// Coverts city names to coordinates
+// https://api.openweathermap.org/data/2.5/forecast?q={city name}&appid=b98ec477e026dbcba46222f669c18788
+
 // TODO:
 // when user searches for a city (clicks search button):
 //  - store the user input in a variable
 //  - use a fetch api to get the current & future conditions for that city
 //  - store that city into local storage
+
+//Function to get weather data from apiUrl
+function getWeather(city) {
+  // Click event for search button
+  searchButton.addEventListener(`click`, function () {
+    // Makes first letter of the user input capital
+    var str = searchTextEl.value;
+    var str2 = str.charAt(0).toUpperCase() + str.slice(1);
+    console.log(str2);
+
+    // API URL for city
+    var apiCityUrl = openWeatherCityUrl + str2 + '&appid=' + openWeatherApiKey;
+    
+    fetch(apiCityURL)
+      .then(function (response) {
+        response.json().then(function(data){
+
+        })
+      })
+
+})
+}
+
+
 
 // use the data from fetch to populate in the current-weather container:
 //  - name and today's date as M/DD/YYY
@@ -19,20 +64,6 @@
 
 //  - when you click the button it displays the current and future conditions for that city
 
-// GLOBAL VARIABLES 
-// Selects City Name Input
-var searchTextEl = document.getElementById("cityName");
-console.log(searchTextEl)
-// Selects Search Button
-var searchButton = document.getElementsByClassName("btn-primary");
-// Selects City Name Box Header
-var cityNameInfo = document.getElementById("cityNameInfo").firstChild;
-// Selects list of city name elements
-var cityList = document.getElementById("cityList");
-// Selects 5 Day Forecast Boxes
-var fiveDayBoxes = document.getElementsByClassName("rounded");
-
-
 // Toggles all buttons using Bootstrap
 var buttons = document.querySelectorAll('.btn')
 buttons.forEach(function (button) {
@@ -40,33 +71,30 @@ buttons.forEach(function (button) {
   button.toggle()
 });
 
-// Geocoding API
-// Coverts city names to coordinates
-// api.openweathermap.org/data/2.5/forecast?q={city name}&appid=b98ec477e026dbcba46222f669c18788
 
-var getWeather = () => {
-  // Hard coded in Chicago to test it
-  fetch(api.openweathermap.org/data/2.5/forecast?q=Chicago&appid=b98ec477e026dbcba46222f669c18788)
-  .then(response => response.json())
-  // Don't know what to put here???
-  .then()
-}
+// var getWeather = () => {
+//   // Hard coded in Chicago to test it
+//   fetch(api.openweathermap.org/data/2.5/forecast?q=Chicago&appid=b98ec477e026dbcba46222f669c18788)
+//   .then(response => response.json())
+//   // Don't know what to put here???
+//   .then()
+// }
 
 // Get Weather By City Search Bar
-searchButton.addEventListener(`search`, (event) => {
-  event.preventdefault()
-  var cityNameValue = searchTextEl.value;
+// searchButton.addEventListener(`search`, (event) => {
+//   event.preventdefault()
+//   var cityNameValue = searchTextEl.value;
 
-  console.log(`User requested weather for ${cityNameValue}`)
+//   console.log(`User requested weather for ${cityNameValue}`)
 
-  // Clears everything on the right side
-  searchTextEl.innerHTML = ``;
-  cityNameInfo.innerHTML = ``;
-  cityList.innerHTML = ``;
-  fiveDayBoxes.innerHTML = ``;
+//   // Clears everything on the right side
+//   searchTextEl.innerHTML = ``;
+//   cityNameInfo.innerHTML = ``;
+//   cityList.innerHTML = ``;
+//   fiveDayBoxes.innerHTML = ``;
 
-  // getWeather(`url to get weather`)
+//   // getWeather(`url to get weather`)
 
-})
+// })
 // Get Weather by Clicking a Gray Button
 
